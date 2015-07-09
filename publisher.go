15b87fa86053784f800cc4733948c4c93feb9e88
Camron Levanger <clevanger@insidesales.com>
2015-07-09 17:18:54 -0600

    Go lints things in a standard that was pretty foreign to me, this diff makes clobbermonster it compliant

--- publisher.go
+++ publisher.go
@@ -1,54 +1,54 @@
 package main
 
 import (
-    "encoding/json"
-    "github.com/golang/glog"
-    "github.com/streadway/amqp"
+	"encoding/json"
+	"github.com/golang/glog"
+	"github.com/streadway/amqp"
 )
 
 func Publish(messages []map[string]interface{}, connection string, queue string) {
 
-    var err error
+	var err error
 
-    glog.Infof("Connecting to queue at %s\n", connection)
-    conn, err := amqp.Dial(connection)
-    failOnError(err, "Can't connect to RabbitMQ")
+	glog.Infof("Connecting to queue at %s\n", connection)
+	conn, err := amqp.Dial(connection)
+	failOnError(err, "Can't connect to RabbitMQ")
 
-    defer conn.Close()
+	defer conn.Close()
 
-    ch, err := conn.Channel()
-    failOnError(err, "Can't get channel")
+	ch, err := conn.Channel()
+	failOnError(err, "Can't get channel")
 
-    defer ch.Close()
+	defer ch.Close()
 
-    q, err := ch.QueueDeclare(
-        queue, // name
-        true,  // durable
-        false, // delete when usused
-        false, // exclusive
-        false, // no-wait
-        nil,   // arguments
-    )
-    if err != nil {
-        failOnError(err, "Can't declare queue")
-    }
+	q, err := ch.QueueDeclare(
+		queue, // name
+		true,  // durable
+		false, // delete when usused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	if err != nil {
+		failOnError(err, "Can't declare queue")
+	}
 
-    for _, message := range messages {
+	for _, message := range messages {
 
-        msg, _ := json.Marshal(message)
+		msg, _ := json.Marshal(message)
 
-        err = ch.Publish(
-            "",     // exchange
-            q.Name, // routing key
-            false,  // mandatory
-            false,  // immediate
-            amqp.Publishing{
-                ContentType: "text/plain",
-                Body:        []byte(msg),
-            })
-        if err != nil {
-            failOnError(err, "Unable to publish, shutting down...")
-        }
-    }
+		err = ch.Publish(
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(msg),
+			})
+		if err != nil {
+			failOnError(err, "Unable to publish, shutting down...")
+		}
+	}
 
 }
